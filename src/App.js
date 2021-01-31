700673517884c1663eaf67a467dc1ba7b1b817f9
dm1tro69 <dimolg22@gmail.com>
2021-01-31 18:29:06 +0200

    finished

--- src/App.js
+++ src/App.js
@@ -8,17 +8,39 @@ const searchUrl = `https://api.unsplash.com/search/photos/`
 function App() {
   const [loading, setLoading] = useState(false)
   const [photos, setPhotos] = useState([])
+  const [page, setPage] = useState(0)
+  const [query, setQuery] = useState('')
 
   const fetchImage = async () => {
     setLoading(true)
     let url;
-    url = `${mainUrl}${clientID}`
+    const urlPage = `&page=${page}`
+    const urlQuery = `&query=${query}`
+
+    if(query){
+      url = `${searchUrl}${clientID}${urlPage}${urlQuery}`
+    }else {
+      url = `${mainUrl}${clientID}${urlPage}`
+
+    }
+
 
   try{
     const response = await fetch(`${url}`)
     const data = await response.json()
+    setPhotos((oldPhotos)=> {
+      if (query && page === 1){
+        return data.results
+      }
+      else if (query){
+        return [...oldPhotos, ...data.results]
+      }else {
+        return [...oldPhotos, ...data]
+
+      }
+    })
     setLoading(false)
-    console.log(data)
+
   }catch (e) {
       setLoading(false)
     console.log(e)
@@ -26,9 +48,41 @@ function App() {
   }
   useEffect(()=> {
     fetchImage()
+  }, [page])
+
+  useEffect(()=> {
+    const event = window.addEventListener('scroll', ()=> {
+      if (!loading && window.innerHeight + window.scrollY >= document.body.scrollHeight - 2){
+          setPage((prevPage)=> {return prevPage + 1})
+      }
+    })
+    return ()=> window.removeEventListener('scroll', event)
   }, [])
 
-  return <h2>stock photos starter</h2>
+  const handleSubmit = (e) => {
+     e.preventDefault()
+    setPage(1)
+  }
+
+  return <main>
+    <section className="search">
+      <form className="search-form">
+        <input value={query} onChange={(e)=> {setQuery(e.target.value)}} className={'form-input'} type="text" placeholder={'search'}/>
+        <button onClick={handleSubmit} type={'submit'} className="submit-btn"><FaSearch/></button>
+      </form>
+    </section>
+    <section className="photos">
+      <div className="photos-center">
+        {photos.map((photo)=> {
+          return (
+              <Photo key={photo.id} {...photo}/>
+
+          )
+        })}
+      </div>
+      {loading && <h2 className={'loading'}>Loading...</h2>}
+    </section>
+  </main>
 }
 
 export default App
--- src/Photo.js
+++ src/Photo.js
@@ -1,7 +1,16 @@
 import React from 'react'
 
-const Photo = () => {
-  return <h2>photo component</h2>
+const Photo = ({urls:{regular}, likes, user:{name, portfolio_url, profile_image:{medium}}}) => {
+  return <article className={'photo'}>
+    <img src={regular} alt="img"/>
+    <div className="photo-info">
+      <div>
+        <h4>{name}</h4>
+        <p>{likes} likes</p>
+      </div>
+      <a href={portfolio_url}><img className={'user-img'} src={medium} alt="img"/></a>
+    </div>
+  </article>
 }
 
 export default Photo
